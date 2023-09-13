cb451c6c8c662ce7464438a57851662af00b8cd4
ynonp <ynonperek@gmail.com>
2023-09-13 13:09:15 +0300

    add info extraction example

--- day3/textprocessing.py
+++ day3/textprocessing.py
@@ -7,11 +7,12 @@ HELLO - but I'm ok
 Im a line that shouldn't be printed
 BUT I'm a line that should""")
 
-pat = re.compile(r'^[A-Z]+\b')
+pat = re.compile(r'(?P<firstword>^[A-Z]+)\b')
 
 for line in text:
-    if pat.search(line):
+    if match := pat.search(line):
         print(line)
+        print("First word is: ", match.groupdict()['firstword'])
 
 
 def rename_by_pattern(input_format: str, output_format: str, filenames: list[str]) -> list[str]:
